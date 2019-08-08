10ce2d771120a7e1a84809d30a2b4cff2da0c8ac
Karina <karinaanton77@mail.com>
2019-08-08 12:45:55 +0200

    deleted commentaries and changed tags places

--- web_gui.py
+++ web_gui.py
@@ -7,15 +7,11 @@ class MyGuy:
     def __init__(self):
         self.root = tk.Tk()
 
-        # Label(text="⬤", fg='red').grid(row=0, column=0)
-        # Label(text="⬤", fg='yellow').grid(row=1, column=0)
-        # Label(text="⬤", fg='green').grid(row=2, column=0)
+        self.redState = Label(text="⬤", fg='red', font=("Courier", 100))
+        self.redState.grid(row=0, column=0)
 
-        self.greenState = Label(text="⬤", fg='green', font=("Courier", 144))
-        self.greenState.grid(row=0, column=0)
-
-        self.yellowState = Label(text="⬤", fg='yellow', font=("Courier", 144))
+        self.yellowState = Label(text="⬤", fg='yellow', font=("Courier", 100))
         self.yellowState.grid(row=1, column=0)
 
-        self.redState = Label(text="⬤", fg='red', font=("Courier", 144))
-        self.redState.grid(row=2, column=0)
+        self.greenState = Label(text="⬤", fg='green', font=("Courier", 100))
+        self.greenState.grid(row=2, column=0)
