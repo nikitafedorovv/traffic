2a6f9b51ff5e247bb8549c9dbabac229554c4639
sergeyis <sergey.ivanovich.sidorov@gmail.com>
2019-08-08 11:39:44 +0100

    amazing improvement

--- web_gui.py
+++ web_gui.py
@@ -11,11 +11,11 @@ class MyGuy:
         # Label(text="⬤", fg='yellow').grid(row=1, column=0)
         # Label(text="⬤", fg='green').grid(row=2, column=0)
 
-        self.greenState = Label(text="⬤", fg='green')
+        self.greenState = Label(text="⬤", fg='green', font=("Courier", 144))
         self.greenState.grid(row=0, column=0)
 
-        self.yellowState = Label(text="⬤", fg='yellow')
+        self.yellowState = Label(text="⬤", fg='yellow', font=("Courier", 144))
         self.yellowState.grid(row=1, column=0)
 
-        self.redState = Label(text="⬤", fg='red')
+        self.redState = Label(text="⬤", fg='red', font=("Courier", 144))
         self.redState.grid(row=2, column=0)
