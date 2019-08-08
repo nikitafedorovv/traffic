2cb8c5bc14d92c9c8234f64a4837d26f6a35503b
Nikita <n@n.n>
2019-08-08 10:57:04 +0100

    Implement get_state()

---
+++ config.py
@@ -0,0 +1,5 @@
+STATES_CONFIG = {'100': {'next_state': [1, 1, 0], 'delay': 4},
+                 '110': {'next_state': [0, 0, 1], 'delay': 2},
+                 '001': {'next_state': [0, 1, 1], 'delay': 4},
+                 '011': {'next_state': [1, 0, 0], 'delay': 2}
+                 }
--- traffic_light.py
+++ traffic_light.py
@@ -1,10 +1,16 @@
-import random
+import time
+from util import *
+from config import STATES_CONFIG
 
 
 class TrafficLight:
+    __state = [1, 0, 0]
+
     def __init__(self):
         pass
 
     def get_state(self):
-        return [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)]
-
+        state_config = STATES_CONFIG[as_str(self.__state)]
+        time.sleep(state_config['delay'])
+        self.__state = as_array(state_config['next_state'])
+        return self.__state
